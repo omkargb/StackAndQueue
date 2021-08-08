ec37e9c037ac864d1936781886f53f0b4d6af481
omkargb <omkargb107@gmail.com>
2021-08-08 22:30:04 +0530

    [omkargb] Add : Enque and display operation on queue implemented.

---
+++ StackAndQueueProject/LinkedListQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueueProject
+{
+    class LinkedListQueue
+    {
+        Node head;
+        public void EnQueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node newNode = head;
+                while (newNode.next != null)
+                {
+                    newNode = newNode.next;
+                }
+                newNode.next = node;
+            }
+            Console.WriteLine(" Element [" + node.data + "] inserted into the Queue.");
+        }
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine(" Queue is Empty... ");
+            }
+            else
+            {
+                Node newNode = head;
+                while (newNode.next != null)
+                {
+                    Console.Write("\t"+ newNode.data);
+                    newNode = newNode.next;
+                }
+                Console.Write("\t"+newNode.data);
+            }
+        }
+
+    }
+}
--- StackAndQueueProject/Program.cs
+++ StackAndQueueProject/Program.cs
@@ -8,13 +8,23 @@ namespace StackAndQueueProject
         {
             Console.WriteLine(" Welcome to Stack and Queue implementation program \n");
 
-            LinkedListStack stak = new LinkedListStack();
+            LinkedListQueue llqueue = new LinkedListQueue();
+
+            //add elements to queue -- enqueue
+            llqueue.EnQueue(56);
+            llqueue.EnQueue(30);
+            llqueue.EnQueue(70);
+
+            //dispaly queue elements
+            Console.Write(" Queue elements : [Top] →");
+            llqueue.Display();
+
             //adding elements to stack
-            Console.WriteLine(" Adding Elements to stack... - Last Element is 'TOP'.");
-            stak.Push(70);
-            stak.Push(30);
-            stak.Push(56);
-            stak.IsEmpty();
+            //Console.WriteLine(" Adding Elements to stack... - Last Element is 'TOP'.");
+            //stak.Push(70);
+            //stak.Push(30);
+            //stak.Push(56);
+            //stak.IsEmpty();
         }
     }
 }
